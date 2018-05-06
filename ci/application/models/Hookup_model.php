34b0cd1c530a3707100d3a0118940452fbdf7e0b
AllanJeremy <aj.dev254@gmail.com>
2018-05-07 02:39:23 +0300

    Added models base code - Renamed the models that existed to follow Codeigniter's php style guide. - Added base code for the models. All we need to do now is add our functions in the models

---
+++ ci/application/models/Hookup_model.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+//This class handles hookups as well as hookup pools
+class Hookup_model extends CI_Model
+{
+    function __construct()
+    {
+        $this->load->database;
+    }
+
+    //Hookup related functions go here
+}
---
+++ ci/application/models/User_model.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+//This class handles user related operations
+class User_model extends CI_Model
+{
+    function __construct()
+    {
+        $this->load->database;
+    }
+
+    //User related functions go here
+}
