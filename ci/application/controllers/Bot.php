adc83a2d3a233f320360f6b2d0490f7955346fc0
AllanJeremy <aj.dev254@gmail.com>
2018-05-21 16:10:57 +0300

    Moved base bot init logic to controller - This is because this is the logic that will be constant for the bot the whole time. Anything added into the test view is going to be extra now

--- ci/application/controllers/Bot.php
+++ ci/application/controllers/Bot.php
@@ -2,9 +2,30 @@
 
 class Bot extends CI_Controller{
 
+    //Initialize bot ~ handles how functions go
+    protected function init_bot()
+    {
+        $this->load->library('commands/cmd_handler');# Load the command handler
+
+        $user_input = $this->telegram->get_user_update();
+        $chat_id = is_object($user_input) ? $user_input->message->chat->id : '';
+        $msg_text = is_object($user_input) ? $user_input->message->text : '';
+        
+        //If the message is a reply ~ handle the reply
+        $this->load->library('telegram/reply_handler');
+        if($this->reply_handler->is_reply($msg_text))
+        {   $cmd_str = $this->reply_handler->get_command_string($msg_text);  }
+        else //Otherwise just handle the command normally
+        {   $cmd_str = $msg_text;  }
+        
+        $commands = $this->cmd_parser->parse($cmd_str);
+        $this->cmd_handler->handle_command($commands);
+    }
+
     //Test the view
     function index()
     {
+        $this->init_bot();
         $this->load->view('test');
     }
 
--- ci/application/views/test.php
+++ ci/application/views/test.php
@@ -1,19 +1,3 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 $ci = &get_instance();
-$ci->load->library('commands/cmd_handler');
-
-$user_input = $ci->telegram->get_user_update();
-$chat_id = is_object($user_input) ? $user_input->message->chat->id : '';
-$msg_text = is_object($user_input) ? $user_input->message->text : '';
-
-
-//If the message is a reply ~ handle the reply
-$ci->load->library('telegram/reply_handler');
-if($ci->reply_handler->is_reply($msg_text))
-{   $cmd_str = $ci->reply_handler->get_command_string($msg_text);  }
-else //Otherwise just handle the command normally
-{   $cmd_str = $msg_text;  }
-
-$commands = $ci->cmd_parser->parse($cmd_str);
-$ci->cmd_handler->handle_command($commands);
