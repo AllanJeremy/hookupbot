aecb3d0fe1c19f913cf0cade2b125de61f1893bb
AllanJeremy <aj.dev254@gmail.com>
2018-05-24 13:43:33 +0300

    Bot now gets callback responses - If a user clicks on a button with a callback, the bot will get the command that should be executed by the callback. - This also led to choosing  between getting data from the udpate object directly or from the callback query

--- ci/application/controllers/Bot.php
+++ ci/application/controllers/Bot.php
@@ -8,8 +8,10 @@ class Bot extends CI_Controller{
         $this->load->library('commands/cmd_handler');# Load the command handler
 
         $user_input = $this->telegram->get_user_update();
-        $chat_id = is_object($user_input) ? $user_input->message->chat->id : '';
-        $msg_text = is_object($user_input) ? $user_input->message->text : '';
+        $cb_query = &$user_input->callback_query;//Callback query if any
+        
+        $chat_id = isset($cb_query) ? $cb_query->message->chat->id : $user_input->message->chat->id;//If there is callback data, get the chat id from there
+        $msg_text = isset($cb_query) ? $cb_query->data : $user_input->message->text; //If there is callback data, get the message text from the callback query
         
         //If the message is a reply ~ handle the reply
         $this->load->library('telegram/reply_handler');
@@ -18,6 +20,7 @@ class Bot extends CI_Controller{
         else //Otherwise just handle the command normally
         {   $cmd_str = $msg_text;  }
         
+        $this->telegram->debug_message('Command string : '.json_encode($cmd_str));#Debug
         $commands = $this->cmd_parser->parse($cmd_str);
         $this->cmd_handler->handle_command($commands);
     }
