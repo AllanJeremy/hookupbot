0cfe6b3a26dc9f0cd47b34374a20ff4815be2388
AllanJeremy <aj.dev254@gmail.com>
2018-05-25 23:33:34 +0300

    Added base keyboard functions - TODO: Implement these functions that allow for a keyboard to be created, the keyboard can then be parsed into the send_message methods as an extra parameter - TODO: Change to using curl instead of file_get_contents for Telegram library

--- ci/application/libraries/telegram/Keyboard.php
+++ ci/application/libraries/telegram/Keyboard.php
@@ -9,4 +9,56 @@ class Keyboard
     {
         $this->ci = &get_instance();
     }
+    /* 
+        HELPER FUNCTIONS
+    */
+
+    //Set an option 
+    public function set_opt($param_name,$value)
+    {
+        return  json_encode(array(
+            $param_name => value
+        ));
+    }
+    
+    /* 
+        KEYBOARDS
+    */
+    //Inline keyboard ~ returns an inline keyboard object
+    public function inline_keyboard(array $buttons)
+    {
+        //TODO: Add implementation
+    }
+
+    //Reply keyboard ~ returns a reply keyboard object
+    public function reply_keyboard(array $buttons)
+    {
+        //TODO: Add implementation
+    }
+
+    //Reply keyboard remove ~ returns a reply_keyboard remove object
+    public function reply_keyboard_remove(bool $should_remove=TRUE)
+    {
+        //TODO: Add implementation
+    }
+    
+    //Force reply ~ returns a force reply object
+    public function force_reply(bool $should_force=TRUE)
+    {
+        //TODO: Add implementation
+    }
+   /* 
+        KEYBOARD BUTTONS
+    */
+    //Inline keyboard button ~ returns a single inline keyboard button
+    public function inline_button(string $text,array $optional)
+    {#Optional must be exactly one item. If not, only the first item will be considered
+        //TODO: Add implementation
+    }
+
+    //Keyboard button ~ returns a single keyboard button
+    public function button(string $text,array $optional)
+    {#Optional must be exactly one item. If not, only the first item will be considered
+        //TODO: Add implementation
+    }
 }
