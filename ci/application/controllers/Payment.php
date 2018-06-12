eb073e5c6cb50faedb32d50a091dfbaf70865d6d
AllanJeremy <aj.dev254@gmail.com>
2018-06-12 22:28:11 +0300

    Added payment controller Mpesa payments will pass through here for validation

---
+++ ci/application/controllers/Payment.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+//Payments will pass through here
+class Payment extends CI_Controller
+{
+    function __construct()
+    {
+
+    }
+
+    //Default route for this controller
+    function index()
+    {
+
+    }
+
+    // Verify a payment and add it to the database based on the $data provided
+    function verify_payment($data)
+    {
+        
+    }
+}
